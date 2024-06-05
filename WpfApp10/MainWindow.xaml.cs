f532d11cbd7d4274e595315e9fc91c0e714e1d03
Stuti Dutta <stutidutta@aarkglobalinc.com>
2024-06-05 10:56:20 +0530

    Add project files.

---
+++ WpfApp10/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            DataContext = this;
+            entries = new ObservableCollection<string>();
+            InitializeComponent();
+            
+        }
+
+        private ObservableCollection<string> entries;
+
+        public ObservableCollection<string> Entries
+        {
+            get { return entries; }
+            set { entries = value; }
+        }
+
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Entries.Add(txtEntry.Text);
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            string selectItem=(string)lvEntries.SelectedItem;
+            Entries.Remove(selectItem);
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            Entries.Clear();
+        }
+    }
+}
